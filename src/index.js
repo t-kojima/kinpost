8752e0c99bec97c70b899701c711ca3ae37eb2d1
t-kojima <t-kojima@skullware.net>
2018-07-16 02:27:35 +0900

    add: validate

--- src/index.js
+++ src/index.js
@@ -1,44 +1,81 @@
+/* eslint-disable no-await-in-loop, no-console, no-restricted-syntax */
+
 const customize = require('./api/customize');
-const fileapi = require('./api/file');
+const fileApi = require('./api/file');
 const { loadFile } = require('./file');
 
 const green = text => `\u001b[32m${text}\u001b[0m`;
 const red = text => `\u001b[31m${text}\u001b[0m`;
 
+const validate = (schema, params) => {
+  function* filenames() {
+    const items = R.xprod(['desktop', 'mobile'], ['js', 'css']).map(
+      a => schema[a[0]][a[1]]
+    );
+    const files = [].concat(...items).filter(item => item.type === 'FILE');
+    yield* files.map(file => file.file.name);
+  }
+
+  if (
+    !params.overwrite &&
+    [...filenames()].some(name =>
+      params.files.some(file => basename(file.name === name))
+    )
+  ) {
+    throw Error();
+  }
+};
+
 /**
  * カスタムJavaScriptファイルをアップロードする
- * @param param.domain kintoneサブドメイン
- * @param param.app 対象アプリID
- * @param param.username ユーザー名
- * @param param.password パスワード
- * @param param.file.path ファイルパス
- * @param param.file.encoding エンコード(default: utf-8)
- * @param param.file.type js or css(default: js)
- * @param param.platform desktop or mobile(default: desktop)
+ * @param {string} param.domain kintoneサブドメイン
+ * @param {number} param.app 対象アプリID
+ * @param {string} param.username ユーザー名
+ * @param {string} param.password パスワード
+ * @param {Array.Object} param.files ファイル情報
+ * @param {string} [param.platform=desktop] desktop or mobile
+ * @param {boolean} [param.overwrite=false] 上書き有無
  */
 module.exports = async params => {
   try {
     const auth = {
       domain: params.domain,
       username: params.username,
-      password: params.password
+      password: params.password,
     };
 
-    const file = await loadFile(params.file.path, params.file.encoding);
-    const fileKey = await fileapi.upload(auth, file);
     const schema = await customize.get(auth, params.app);
-    // TODO remove FILE
-    schema.desktop.js.push({
-      type: 'FILE',
-      file: {
-        fileKey,
-        name: file.name,
-        size: file.size
-      }
-    });
-    schema.app = params.app;
-    schema.revision = -1;
-    await customize.put(auth, schema);
+
+    for (const fileParam of params.files) {
+      const fileObj = await loadFile(fileParam.path, fileParam.encoding);
+      const fileKey = await fileApi.upload(auth, fileObj);
+      // TODO remove FILE
+      schema.desktop.js.push({
+        type: 'FILE',
+        file: {
+          fileKey,
+          name: fileObj.name,
+          size: fileObj.size,
+        },
+      });
+      schema.app = params.app;
+      await customize.put(auth, schema);
+    }
+    // const file = await loadFile(params.file.path, params.file.encoding);
+    // const fileKey = await fileApi.upload(auth, file);
+    // const schema = await customize.get(auth, params.app);
+    // // TODO remove FILE
+    // schema.desktop.js.push({
+    //   type: 'FILE',
+    //   file: {
+    //     fileKey,
+    //     name: file.name,
+    //     size: file.size,
+    //   },
+    // });
+    // schema.app = params.app;
+    // schema.revision = -1;
+    // await customize.put(auth, schema);
     console.info(green('File Upload Successfully.'));
   } catch (e) {
     console.error(
---
+++ src/validate.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-restricted-syntax */
+
+const R = require('ramda');
+const { basename } = require('path');
+
+function* filenames(schema) {
+  const items = R.xprod(['desktop', 'mobile'], ['js', 'css']).map(
+    a => schema[a[0]][a[1]]
+  );
+  const files = [].concat(...items).filter(item => item.type === 'FILE');
+  yield* files.map(file => file.file.name);
+}
+
+module.exports = (params, schema) => {
+  if (!params.overwrite) {
+    for (const filename of [...filenames(schema)]) {
+      if (params.files.some(file => basename(file.path) === filename)) {
+        throw Error();
+      }
+    }
+  }
+};
