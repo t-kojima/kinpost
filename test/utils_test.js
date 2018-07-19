24392a5d135536c0cb7a483452fd248098b50f60
t-kojima <t-kojima@skullware.net>
2018-07-19 12:08:18 +0900

    feat: Append 4 validation and test.

--- test/utils_test.js
+++ test/utils_test.js
@@ -3,43 +3,87 @@ const { assert, expect } = require('chai');
 const { validate } = require('../lib/utils');
 
 describe('validation', () => {
-  it('return true when default', () => {
-    assert.isTrue(
-      validate({
-        files: [{ path: './missing.txt' }],
-      })
-    );
+  describe('domain', () => {
+    it('throw an error when missing domain', () => {
+      expect(() => validate({})).to.throw();
+    });
   });
 
-  it('return true when desktop and css', () => {
-    assert.isTrue(
-      validate({
-        files: [{ path: './missing.txt', platform: 'desktop', type: 'css' }],
-      })
-    );
+  describe('app', () => {
+    it('throw an error when missing app', () => {
+      expect(() => validate({ domain: 'hoge' })).to.throw();
+    });
+
+    it('throw an error when app is not Number', () => {
+      expect(() => validate({ domain: 'hoge', app: 'fuga' })).to.throw();
+    });
   });
 
-  it('throw an error when xbox', () => {
-    expect(() =>
-      validate({
-        files: [{ path: './missing.txt', platform: 'xbox', type: 'css' }],
-      })
-    ).to.throw();
+  describe('username', () => {
+    it('throw an error when missing username', () => {
+      expect(() => validate({ domain: 'hoge', app: 0 })).to.throw();
+    });
   });
 
-  it('throw an error when html', () => {
-    expect(() =>
-      validate({
-        files: [{ path: './missing.txt', platform: 'mobile', type: 'html' }],
-      })
-    ).to.throw();
+  describe('password', () => {
+    it('throw an error when missing username', () => {
+      expect(() =>
+        validate({ domain: 'hoge', app: 0, username: 'fuga' })
+      ).to.throw();
+    });
   });
 
-  it('throw an error when mobile and css', () => {
-    expect(() =>
-      validate({
-        files: [{ path: './missing.txt', platform: 'mobile', type: 'css' }],
-      })
-    ).to.throw();
+  describe('files', () => {
+    const auth = {
+      domain: 'hoge',
+      app: 0,
+      username: 'fuga',
+      password: 'piyo',
+    };
+
+    it('return true when default', () => {
+      assert.isTrue(
+        validate({
+          ...auth,
+          files: [{ path: './missing.txt' }],
+        })
+      );
+    });
+
+    it('return true when desktop and css', () => {
+      assert.isTrue(
+        validate({
+          ...auth,
+          files: [{ path: './missing.txt', platform: 'desktop', type: 'css' }],
+        })
+      );
+    });
+
+    it('throw an error when xbox', () => {
+      expect(() =>
+        validate({
+          ...auth,
+          files: [{ path: './missing.txt', platform: 'xbox', type: 'css' }],
+        })
+      ).to.throw();
+    });
+
+    it('throw an error when html', () => {
+      expect(() =>
+        validate({
+          ...auth,
+          files: [{ path: './missing.txt', platform: 'mobile', type: 'html' }],
+        })
+      ).to.throw();
+    });
+
+    it('throw an error when mobile and css', () => {
+      expect(() =>
+        validate({
+          ...auth,
+          files: [{ path: './missing.txt', platform: 'mobile', type: 'css' }],
+        })
+      ).to.throw();
+    });
   });
 });
